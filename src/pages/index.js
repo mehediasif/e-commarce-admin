2a579acaf849ee6da13740cbb0660869a849ed1a
Mohammad Mehadi Hasan <mohhas@st.amu.edu.pl>
2023-04-23 07:35:41 +0200

    New routes configured

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,24 @@
+import { useSession } from "next-auth/react";
 import Layout from "../../components/Layout";
+import Image from "next/image";
 
 export default function Home() {
+  const { data: session } = useSession()
+  if(!session) return;
+
   return(
     <Layout>
-    Home DashBoard
+    <div className="text-slate-700 flex justify-between">
+      <h3>
+      Hello, {session?.user?.email}
+      </h3>
+      <div className="flex bg-gray-500 gap-2 text-blue-700 rounded-md space-y-px">
+        <img src={session?.user?.image} alt='' className="w-16 h-16 overflow-hidden rounded-sm" />
+        <span className="px-2 py-2">
+          {session?.user?.name}
+        </span>
+      </div>
+    </div>
     </Layout>  
   )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import Layout from "../../components/Layout";
 
 export default function Products() {
   return(
     <Layout>
-    Products Page here
+      <Link className="bg-cyan-700 text-black rounded-md py-1 px-2" href={'/products/new'}>Add New Product</Link>
     </Layout>  
   )
 }
